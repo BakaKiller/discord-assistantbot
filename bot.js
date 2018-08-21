5137516e8acf72eed82c8da1aa630a43eaa22b3c
Biquet <Baka-killer@hotmail.fr>
2018-08-21 23:59:05 +0200

    made ping and sign functional

--- bot.js
+++ bot.js
@@ -1,28 +1,60 @@
 #!/usr/bin/env node
 
 const Discord = require('discord.js');
-const jsonfile = require('jsonfile');
 const client = new Discord.Client();
 const config = require('./config.js');
 
+let debugchan;
+let prefix;
+let message;
+let messageparts;
+let text;
+
+
 config.on('ready', () => {
-    let prefix = config.prefix;
+    prefix = config.prefix;
     client.login(config.token);
 });
 
 client.on('ready', () => {
+    debugchan = client.channels.get(config.debugchan);
+
     console.log(`Logged in as ${client.user.tag}!`);
-    sign(client.guilds.get('479266358304178177'), client.users.get("139512885679357953"));
+    client.on('message', (msg) => {
+        if (msg.content.substr(0, prefix.length) === prefix) {
+            message = (msg.content.substr(prefix.length)).toLowerCase();
+            messageparts = message.split(' ');
+            switch (messageparts[0]) {
+                case "ping":
+                    if (msg.member === null || is_admin(msg.member)) {
+                        msg.reply('Pong ! (`' + (new Date().getTime() - msg.createdTimestamp) + 'ms`)');
+                    } else {
+                        debugchan.send('Not an admin ? :r');
+                    }
+                    break;
+                case "sign":
+                    if (msg.member !== null) {
+                        sign(msg.member);
+                    }
+                    msg.delete();
+            }
+        }
+    })
 });
 
-// LIB ============================================================================
-
-let sign = (guild, user) => {
-    let chan = client.channels.get(config.debugchan);
+function sign(guildmember) {
+    let guild = guildmember.guild;
+    let user = guildmember.user;
     if (!user instanceof Discord.User) {
-        chan.send('User is not a User (function \'sign\')');
+        debugchan.send('User is not a User (function \'sign\')');
         return;
     }
-    let memberrole = guild.roles.get(config.roles.Members);
-    console.log(config.roles);
-};
+    let memberrole = guild.roles.get(config.roles.Member);
+    if (!guildmember.roles.has(memberrole)) {
+        guildmember.addRole(memberrole);
+    }
+}
+
+function is_admin(guildmember) {
+    return guildmember.roles.has(config.roles.Admin);
+}
