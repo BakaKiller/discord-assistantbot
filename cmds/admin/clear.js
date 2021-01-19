698c7fd7a4dd4ad9d16d55b278e3cd36c74d58b0
BakaKiller <crowd@bakakiller.ovh>
2021-01-19 14:25:47 +0100

    Add clear cmd

--- cmds/admin/clear.js
+++ cmds/admin/clear.js
@@ -22,6 +22,6 @@ module.exports = class ClearCommand extends Command {
 
     run(message, {nb}) {
         let chan = message.channel;
-        chan.fetchMessages({limit: nb}).then(messages => messages.forEach(msg => msg.delete()))
+        chan.messages.fetch({limit: nb}).then(messages => messages.forEach(msg => msg.delete()))
     }
 }
